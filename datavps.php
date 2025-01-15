f6e3f39c7e590120142e63e90f9a6bab5f9019ba
baru0811 <barudatang02@gmail.com>
2025-01-15 21:49:44 +0700

    Add files via upload

---
+++ datavps.php
@@ -0,0 +1,10 @@
+// masukan data vps anda dengan benar
+
+username : root
+ip vps : 126.26.12.10
+password vps :daffnes16gb
+
+auth/vps.php
+daffanesiaserver/auth/web/phising.html
+
+// connection ke vps
